7dcbff679c1dc808e63127a006057b1a93e90601
Maija Lintunokka <maija.lintunokka@nosto.com>
2025-08-22 10:20:29 +0300

    chore: exclude configs from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
-      exclude: ["node_modules/", "test/", "vitest.config.ts", "src/bootstrap.sh"]
+      exclude: ["node_modules/", "test/", "vitest.config.ts", "src/bootstrap.sh", "*.config.js"]
     }
   },
   esbuild: {
