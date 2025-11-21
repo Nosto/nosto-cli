f70e88627c456591a51af5779c2d1e43da093972
Tomas Jaseliūnas <tomas.jaseliunas@Mac.lan>
2025-11-21 17:24:54 +0200

    feat: deployment listings

--- src/filesystem/homeDirectory.ts
+++ src/filesystem/homeDirectory.ts
@@ -1,3 +1,3 @@
 import os from "os"
 
-export const HomeDirectory = import.meta.env.MODE === "test" ? "/vitest/home" : os.homedir()
+export const HomeDirectory = import.meta.env?.MODE === "test" ? "/vitest/home" : os.homedir()
