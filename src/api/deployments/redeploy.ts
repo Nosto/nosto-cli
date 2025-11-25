9690bb4df5b306edd28c3c4b12260cebd999d45b
Tomas Jaseliūnas <tomas.jaseliunas@Mac.lan>
2025-11-25 16:27:44 +0200

    fix: json headers all

--- src/api/deployments/redeploy.ts
+++ src/api/deployments/redeploy.ts
@@ -1,11 +1,11 @@
 import ky from "ky"
 
-import { getHeaders, getSourceUrl } from "#api/utils.ts"
+import { getHeaders, getJsonHeaders, getSourceUrl } from "#api/utils.ts"
 
 export async function redeploy(deploymentId: string) {
   const url = getSourceUrl(`deployment/{env}/${deploymentId}`)
 
   await ky.post(url, {
-    headers: getHeaders()
+    headers: getJsonHeaders()
   })
 }
